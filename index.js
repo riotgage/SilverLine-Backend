e36015f430fdb4b71f837987f9d8c72e0447a219
AstonX <yashsali7@gmail.com>
2021-06-12 09:09:00 +0530

    payment backend updated

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express=require("express")
-const app = express()
-const authRoutes=require("./routes/authRoutes")
-const fundraiserRoutes=require("./routes/fundraiserRoutes")
-const paymentRoutes=require("./routes/paymentRoute")
-const PORT=process.env.PORT || 5000;
-const {errorHandler}=require("./middleware/error")
-const connectDB=require("./config/db")
-const dotenv=require("dotenv");
+const express = require("express");
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+const fundraiserRoutes = require("./routes/fundraiserRoutes");
+const paymentRoutes = require("./routes/paymentRoute");
+const PORT = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/error");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
 const morgan = require("morgan");
-var cors = require('cors')
+var cors = require("cors");
 
-dotenv.config({path:'./config/config.env'})
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+dotenv.config({ path: "./config/config.env" });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-connectDB()
-app.use(morgan('dev'))
+connectDB();
+app.use(morgan("dev"));
 
-app.use("/api/v1/user/auth",authRoutes);
-app.use("/api/v1/fundraisers",fundraiserRoutes);
-app.use("/api/v1/pay",paymentRoutes);
+app.use("/api/v1/user/auth", authRoutes);
+app.use("/api/v1/fundraisers", fundraiserRoutes);
+app.use("/api/v1/donate", paymentRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("<h1>This is the backend for silverline</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>This is the backend for silverline</h1>");
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const server=app.listen(PORT, function(){
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
+const server = app.listen(PORT, function () {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
 
 //Promise Rejections
-process.on('unhandledRejection',(err,promise)=>{
-    console.log(err)
-    server.close(()=>process.exit(1))    
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(err);
+  server.close(() => process.exit(1));
+});
